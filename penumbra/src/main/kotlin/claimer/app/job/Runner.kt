8884616f4f7a246e6bfa973f1bd4fb7688d1e140
afanasievM <g010203040>
2024-02-17 17:18:44 +0200

    penumbra automated

--- penumbra/src/main/kotlin/claimer/app/job/Runner.kt
+++ penumbra/src/main/kotlin/claimer/app/job/Runner.kt
@@ -4,17 +4,13 @@ import claimer.app.service.PenumbraClaimer
 import claimer.app.service.PenumbraSshService
 import java.time.Duration
 import org.slf4j.LoggerFactory
-import org.springframework.context.annotation.Configuration
-import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
-import reactor.core.publisher.Flux
-import reactor.core.publisher.Mono
 
 @Component
 class Runner(private val claimer: PenumbraClaimer, private val sshService: PenumbraSshService) {
 
-    @Scheduled(fixedRate = 1000000, initialDelay = 0)
+    @Scheduled(cron = "CRON_EXPRESSION")
     fun run() {
         LOG.info("Started Penumbra job")
         claimer.claim()
@@ -25,5 +21,6 @@ class Runner(private val claimer: PenumbraClaimer, private val sshService: Penum
 
     companion object {
         private val LOG = LoggerFactory.getLogger(Companion::class.java)
+        private val CRON_EXPRESSION = "0 0 21 * * *"
     }
 }
