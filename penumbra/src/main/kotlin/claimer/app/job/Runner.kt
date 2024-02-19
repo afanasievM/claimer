1a3048911fc8e60ba18c970c2e490a5f702a6184
afanasievM <g010203040>
2024-02-19 21:11:54 +0200

    try with delay

--- penumbra/src/main/kotlin/claimer/app/job/Runner.kt
+++ penumbra/src/main/kotlin/claimer/app/job/Runner.kt
@@ -15,7 +15,7 @@ class Runner(private val claimer: PenumbraClaimer,
 ) {
 
 //    @Scheduled(cron = CRON_EXPRESSION)
-    @Scheduled(initialDelay = 5000)
+    @Scheduled(initialDelay = 20000)
     fun run() {
         LOG.info("Started Penumbra job")
 
